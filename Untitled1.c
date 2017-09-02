acb1556f15150f95fda5a84e0ec4056b085d0343
giftymagna <31558890+giftymagna@users.noreply.github.com>
2017-09-02 11:32:11 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main
+{
+    int num;
+    printf("Enter a num: ");
+    scanf("%d", &num);
+    if (num <= 0)
+    {
+        if (num == 0)
+            printf("num is zero");
+        else
+            printf("num is negative");
+    }
+    else
+        printf("num is positive");
+    return 0;
+}
